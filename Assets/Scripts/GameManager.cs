020e2f33a506974e4dd8cbf756d006839adb301a
Hektor <hcostaguzman@gmail.com>
2021-11-23 13:01:28 +0100

    Efecto Parallax

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public RawImage background, platform;
+    public float parallaxSpeed = 0.02f;
+
+    void Update()
+    {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+        platform.uvRect = new Rect(platform.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
 }
